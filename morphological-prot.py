0a87fc4efe8462c6a2dd7a073472670c06cc3fa1
watomon <watomon1@gmail.com>
2018-04-18 00:01:07 +0900

    reading

--- morphological-prot.py
+++ morphological-prot.py
@@ -1,16 +1,22 @@
 #author : kawasaki
 
-
 import numpy as np
 import pandas as pd
 
 bun01 = "Time flies like an arrow"  #入力する文章
-part_of_speech_list = ['N', 'V', 'D', 'J']  #品詞のリスト
+c_list = []  #品詞のリスト
+
+#訓練データを文字列データに格納
+f = open('data/sample.train')
+corpus = f.read()  # ファイル終端まで全て読んだデータを返す
+f.close()
+print(corpus) # 文字列データ
 
 #空白を区切りにして、単語別で分解する
-word_list = bun01.split()
+word_list = corpus.split(' ')
 print(word_list)    #出力
 
-
-df = pd.read_csv('test.csv', header = None)
+"""
+df = pd.read_csv('testcorpus.txt', header = None)
 print(df)
+"""
